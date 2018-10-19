30d7a47b8a005142bbbbac0ae0d726c662621f04
pyelika <pyelika@csc.com>
2018-10-19 23:16:48 +0530

    testmethod

--- src/test/java/com/azure/SimpleTest.java
+++ src/test/java/com/azure/SimpleTest.java
@@ -1,5 +1,15 @@
 package com.azure;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
 public class SimpleTest {
 
+	String test = "MavenTest";
+	@Test
+	public void methodTest() {
+		assertTrue(test.equalsIgnoreCase("MavenTest"));
+	}
 }
